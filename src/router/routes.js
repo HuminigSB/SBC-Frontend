0463a518c88c0eb1af591b02f9e53fa675821e4e
humiGod <71161198+humiGod@users.noreply.github.com>
2021-02-18 17:08:26 -0300

    tela adicionar funcionario implementada

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,12 +3,14 @@ import {Switch, Route} from 'react-router-dom'
 
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SingUp'
+import AdicionarFuncionario from '../pages/AdicionarFuncionario'
 
 const Routes = () => {
     return (
         <Switch>
             <Route exact path="/" component={SignIn}/>
             <Route path="/signup" component={SignUp}/>
+            <Route path="/adicionarFuncionario" component={AdicionarFuncionario}/>
         </Switch>
     )
 }
