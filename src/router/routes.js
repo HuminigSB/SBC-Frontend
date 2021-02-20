02926afd98ab18732779dcf2f3bb4d7555573af7
humiGod <71161198+humiGod@users.noreply.github.com>
2021-02-20 16:05:50 -0300

    implementação tela listar usuarios

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom'
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SingUp'
 import AdicionarFuncionario from '../pages/AdicionarFuncionario'
+import ListarUsuarios from '../pages/ListarUsuarios'
 
 const Routes = () => {
     return (
@@ -11,6 +12,7 @@ const Routes = () => {
             <Route exact path="/" component={SignIn}/>
             <Route path="/signup" component={SignUp}/>
             <Route path="/adicionarFuncionario" component={AdicionarFuncionario}/>
+            <Route path="/listarUsuarios" component={ListarUsuarios}/>
         </Switch>
     )
 }
