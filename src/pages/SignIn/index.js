d0e181a3775cec086cec989269265bb3e3034f9b
MatheusINFO <matheus.add12@gmail.com>
2021-02-21 15:40:58 -0300

    Adicionado interface de Login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import {toast} from 'react-toastify'
+import { useForm } from "react-hook-form";
+import api from '../../services/api'
+
+import Logo from '../../assets/logo-white.png'
+
+import { Form, WrapperItens, Input, Button, LinkTo } from './styles'
 
 const SignIn = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        api.post('/login', data).then(function (response){
+            toast.success("Login realizado com sucesso :D")
+        }).catch(function(error){
+            toast.error("Algo deu errado, tente novamente :/")
+        })
+    }
+
     return (
-      <h1>Login</h1>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <img src={Logo} alt="Logo"/>
+
+            <WrapperItens>
+                <Input type="text" name="username" ref={register} placeholder="Nome de usuário"/>
+                <Input type="password" name="password" ref={register} placeholder="Sua senha de acesso"/>
+            </WrapperItens>
+
+            <WrapperItens>
+                <Button type="submit">Entrar</Button>
+                <LinkTo to="/signup">criar conta</LinkTo>
+            </WrapperItens>
+        </Form>
     )
 }
 
